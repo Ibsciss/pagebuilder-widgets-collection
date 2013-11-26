103073e012b3646ab5a0f72b14b7def77398ff84
Arnaud LEMAIRE <alemaire@quantis.fr>
2013-11-26 11:34:39 +0100

    [UPDATE] update separator to new template engine

--- Ibsciss/Wordpress/Resources/views/widgets/separator-form.php
+++ Ibsciss/Wordpress/Resources/views/widgets/separator-form.php
@@ -1,16 +1,10 @@
-<p>
-    <label for="<?php echo $widget->get_field_id( 'separator_width' ); ?>">Width</label>
-    <select id="<?php echo $widget->get_field_id( 'separator_width' ); ?>" name="<?php echo $widget->get_field_name( 'separator_width' ); ?>">
-        <?php for($i = 50; $i <= 100; $i = $i+5): ?>
-            <option <?php if ( $i ==  $separator_width ) echo 'selected="selected"'; ?> value="<?php echo $i ?>"><?php echo $i ?> %</option>
-        <?php endfor;?>
-    </select></p>
+<?php
+$input->Select('Width (%)', range(50, 100, 5), array(
+    'name' => 'separator_width', 
+    'default_value' => 75
+));
 
-<p>
-    <label for="<?php echo $widget->get_field_id( 'text_align' ); ?>">Separator alignment</label>
-    <select id="<?php echo $widget->get_field_id( 'text_align' ); ?>" name="<?php echo $widget->get_field_name( 'text_align' ); ?>">
-        <option <?php if ( 'left' ==  $text_align ) echo 'selected="selected"'; ?> value="left">Left</option>
-        <option <?php if ( 'center' ==  $text_align ) echo 'selected="selected"'; ?> value="center">Center</option>
-        <option <?php if ( 'right' ==  $text_align ) echo 'selected="selected"'; ?> value="right">Right</option>
-    </select>
-</p>
+$input->Select('Text alignment', array('Left', 'Center', 'Right'), array(
+    'name' => 'text_align',
+    'default_value' => 'center'
+));
--- Ibsciss/Wordpress/Resources/views/widgets/separator-widget.php
+++ Ibsciss/Wordpress/Resources/views/widgets/separator-widget.php
@@ -2,5 +2,5 @@
 $left_margin = 100-$separator_width;
 $left_margin = ($text_align == 'center') ? $left_margin/2 : $left_margin;
 $style = ($text_align == 'left') ? '' : 'margin-left:'.$left_margin.'%';
-echo '<hr style="width:'.$separator_width.'%; '.$style.';"/>'.PHP_EOL
+echo '<hr style="width:'.$separator_width.'%; '.$style.';"/>'.PHP_EOL;
 ?>
